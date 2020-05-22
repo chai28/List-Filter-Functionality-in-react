932d926256033e22a787095d38fdaacba42ceaf2
chai28 <clomosbog@gmail.com>
2020-05-22 14:51:50 +1200

    Updates

---
+++ precache-manifest.9af1bd72113a960a0271af0725f15549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6e7342353bb7c35513a00b057c62c7",
+    "url": "/List-Filter-Functionality-in-react/index.html"
+  },
+  {
+    "revision": "d460c8048bf2cc9ffe5f",
+    "url": "/List-Filter-Functionality-in-react/static/css/main.1a61b5ef.chunk.css"
+  },
+  {
+    "revision": "cb05e797adc505cf006c",
+    "url": "/List-Filter-Functionality-in-react/static/js/2.57ae9aa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/List-Filter-Functionality-in-react/static/js/2.57ae9aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d460c8048bf2cc9ffe5f",
+    "url": "/List-Filter-Functionality-in-react/static/js/main.156e364d.chunk.js"
+  },
+  {
+    "revision": "2cc4cd8ccacbbd8b2829",
+    "url": "/List-Filter-Functionality-in-react/static/js/runtime-main.eb1845b1.js"
+  }
+]);
